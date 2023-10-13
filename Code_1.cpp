116deb3ffbfd930a6c645f1ec9e78c70e8409b38
Aman Sharma <amansharma95288@gmail.com>
2023-10-13 19:38:25 +0530

    code till mid term:

--- Code_1.cpp
+++ Code_1.cpp
@@ -1,17 +1,10 @@
-
-// write a program to calculate absolute value ,abstract value,ralative value 
+// write a program to round off a given number..
 #include<iostream>
 using namespace std;
-int main(){
-  float absvalue,absError,trueValue,relativeError,percentError;
-  cout<<"Enter the True value:";
-  cin>>trueValue;
-  cout<<"Enter the Absolute value:";
-  cin>>absvalue;
-  cout<<"So the Absolute error is:"<<absError;
-  relativeError=absError/trueValue;
-  cout<<"Relative Error is :"<<relativeError;
-  percentError=relativeError*100;
-  cout<<"The percentage Error is :"<<percentError;
-  return 0;
+int main (){
+    double x;
+    cout<<"Enter the range:";
+    cin>>x;
+    cout<<round(x)<<endl;
+    return 0;
 }
